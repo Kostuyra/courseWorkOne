ff9a2f458b3b7a75e3ee96b205e400b62def8f4a
Kostuyra <gkv110592@yandex.ru>
2023-04-04 16:17:39 +0300

    Базовая часть

---
+++ src/Employee.java
@@ -0,0 +1,51 @@
+public class Employee {
+	private int id;
+	private static int count;
+	private String fullName;
+	private int partOffice;
+	private double salary;
+
+	public Employee(String fullName, int partOffice, double salary) {
+		this.id = ++ count;
+		this.fullName = fullName;
+		this.partOffice = partOffice;
+		this.salary = salary;
+		addEmployee(this);
+	}
+
+	@Override
+	public String toString() {
+		return "id: " + id +" ФИО: " + fullName + " Отдел № " + partOffice + " Зарплата: " + salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+
+	public int getPartOffice() {
+		return partOffice;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+
+	public void setPartOffice(int partOffice) {
+		this.partOffice = partOffice;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	static void addEmployee(Employee employee) {
+		Main.Employees[employee.getId()] = employee;
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,96 @@
 public class Main {
+	static Employee[] Employees = new Employee[10];
+
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+		Employee empl = new Employee("Ivanov Ivan Semenovich", 1, 25500.0);
+		Employee empl2 = new Employee("Petrov Vasiliy Ivanovich", 2, 50000.0);
+
+	}
+
+	static void allEmployees() {
+		for (Employee i : Employees) {
+			if (isNull(i)) {
+				continue;
+			}
+			System.out.println(i.toString());
+		}
+	}
+
+	static double getSumSalaries() {
+		double sum = 0.0;
+		for (Employee i : Employees) {
+			if (isNull(i)) {
+				continue;
+			}
+			sum += i.getSalary();
+		}
+		return sum;
+	}
+
+	static int getCountEmployees() {
+		int count = 0;
+		for (Employee i : Employees) {
+			if (isNull(i)) {
+				continue;
+			}
+			count++;
+		}
+		return count;
+	}
+
+	static String getEmployeeWithMinSalary() {
+		double salary = 999_999_999_999.0;
+		String nameEmployee = "";
+		for (Employee i : Employees) {
+			if (isNull(i)) {
+				continue;
+			}
+			if (salary > i.getSalary()) {
+				salary = i.getSalary();
+				nameEmployee = i.getFullName();
+			}
+		}
+		return nameEmployee;
+	}
+
+	static String getEmployeeWithMaxSalary() {
+		double salary = 0.0;
+		String nameEmployee = "";
+		for (Employee i : Employees) {
+			if (isNull(i)) {
+				continue;
+			}
+			if (salary < i.getSalary()) {
+				salary = i.getSalary();
+				nameEmployee = i.getFullName();
+			}
+		}
+		return nameEmployee;
+	}
+
+	static double getAvrSalary() {
+		double avr = 0.0;
+		if (getCountEmployees() != 0) {
+			avr = getSumSalaries() / getCountEmployees();
+		}
+		return avr;
+	}
+
+	static void getNameAllEmployees() {
+		for (Employee i : Employees) {
+			if (isNull(i)) {
+				continue;
+			}
+			System.out.println(i.getFullName());
+		}
+	}
+
+	static boolean isNull(Employee employee){
+		boolean boolEmpl = false;
+		if (employee == null) {
+			boolEmpl = true;
+		}
+		return boolEmpl;
 	}
 }
